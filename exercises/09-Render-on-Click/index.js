4e13e48278e5ddb61972e94237499286223d467e
Júlia Valls Moix <juliavalls20@gmail.com>
2023-04-20 18:00:26 +0000

    Almost done

--- exercises/09-Render-on-Click/index.js
+++ exercises/09-Render-on-Click/index.js
@@ -1,6 +1,12 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+	let helloWorld = document.createElement('div');
+	helloWorld.innerHTML = 'Hello World';
+	helloWorld.style.backgroundColor = 'yellow';
+
+	document.body.appendChild(helloWorld);
 	
+
 	
 });
--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -2,4 +2,9 @@ let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
 
+	let liElement = document.createElement('li');
+	liElement.innerHTML = 'Forth element';
+	let newElement = document.querySelector('#myList');
+	newElement.appendChild(liElement);
+
 });
