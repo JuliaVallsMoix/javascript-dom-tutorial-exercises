753de608bda1f0a3904177dd07ee34def6ce518b
Júlia Valls Moix <juliavalls20@gmail.com>
2023-04-21 10:29:09 +0000

    Need Oscar to do the lastone (To do List)

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -9,5 +9,29 @@ function addItem() {
     input.value = '';
 }
 
-input.addEventListener('keydown', )
+input.addEventListener('keydown', function(event) {
+    if (event.keyCode === 13 && input.value !== '') {
+        addItem();
+    )};
 
+    let trash = document.createElement('span');
+    trash.classList.add('trash-icon');
+    trash.textContent = "🗑️"
+    trash.addEventListener("click", function() {
+        list.removeChild(newItem);
+
+        newItem.appendChild(trash);
+    list.appendChild(newItem);
+    newItem.value = ''
+    }
+});
+
+/* Vaig molt perduda, demanar-li a l'Òscar de fer-lo junts. */
+
+
+let trash = document.querySelector('span');
+let task = document.querySelector('li');
+
+/* function deleteItem() {
+    trash.onclick.
+} */
